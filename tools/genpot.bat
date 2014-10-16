81d85f152fe6db83c1c94efa765b9ab6ca32f923
Andre Polykanine A.K.A. Menelion Elensúlë <andre@oire.org>
2014-10-16 20:11:05 +0300

    Cleaning up the requirements file at last

--- tools/genpot.bat
+++ tools/genpot.bat
@@ -1,4 +1,4 @@
 @echo off
 echo Generating application translation strings...
-python pygettext.py -v -d "The Qube" ../src/*.pyw ../src/*.py ../src/*/*.py ../src/*/*.pyw ../src/*/*/*.py ../src/*/*/*.pyw ../src/*/*/*/*.py ../src/*/*/*/*.pyw ../src/*/*/*/*/*.py ../src/*/*/*/*/*.pyw ../src/*/*/*/*/*/*.py ../src/*/*/*/*/*/*.pyw ../src/*/*/*/*/*/*/*.py ../src/*/*/*/*/*/*/*.pyw ../src/*/*/*/*/*/*/*/*.py ../src/*/*/*/*/*/*/*/*.pyw
+python pygettext.py -v -d "TheQube" ../src/*.pyw ../src/*.py ../src/*/*.py ../src/*/*.pyw ../src/*/*/*.py ../src/*/*/*.pyw ../src/*/*/*/*.py ../src/*/*/*/*.pyw ../src/*/*/*/*/*.py ../src/*/*/*/*/*.pyw ../src/*/*/*/*/*/*.py ../src/*/*/*/*/*/*.pyw ../src/*/*/*/*/*/*/*.py ../src/*/*/*/*/*/*/*.pyw ../src/*/*/*/*/*/*/*/*.py ../src/*/*/*/*/*/*/*/*.pyw
 echo Done.
