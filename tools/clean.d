e51152628fec399b351592cffe28ae458b6925d9
Andre Polykanine A.K.A. Menelion Elensúlë <andre@oire.org>
2016-02-11 00:42:30 +0200

    More updates to documentation; updated the cleaner, now it seems to work properly

--- tools/clean.d
+++ tools/clean.d
@@ -1,10 +1,9 @@
 /**
 	TheQube source cleaner
-	Version 0.1
+	Version 0.6
 
 	this command-line utility cleans TheQube source code from byte code and other intermediary files.
-	It optionally takes a path to TheQube source. If the path is missing, it assumes it is situated in TheQube/tools folder, so the source is located at ../src.
-	It also can take an output file name to log its progress.
+	It should be placed in TheQube/tools folder.
 
 	Copyright 2016, Andre Polykanine A.K.A. Menelion Elensúlë
 	https://github.com/Oire
@@ -12,71 +11,42 @@
 
 module theqube.tools.cleaner;
 
-import std.stdio;
 import std.file;
 import std.getopt;
-import parallelism: parallel;
-
-class SourceCleaner {
-	bool verbose;
-	string outFile;
-
-	this(bool verbose, string outFile) {
-		this.verbose = verbose;
-		this.outFile = outFile;
-	}
-
-	final void output(T...)(in string text, in T params) const {
-		if (this.outFile is null || this.outFile == "") {
-			writefln(text, params);
-		} else { // Output to a file
-			auto f = File(this.outFile, "w");
-			scope(exit) f.close();
-			try {
-				f.writefln(text, params);
-			} catch(Exception e) {
-				writefln("Unable to write to %s: %s", this.outFile, e.msg);
-			}
-		}
-	}
-
-	final void clean(in string srcPath) const {
-		try {
-			auto dirIter = dirEntries(srcPath, "{*.pyc,*.pyo,*.obj}", SpanMode.depth);
-			foreach(dirFile; parallel(dirIter, 1)) {
-				try {
-					remove(dirFile);
-					if (this.verbose) {
-						this.output("%s: removed", dirFile);
-					}
-				} catch(Exception e) {
-					this.output("Unable to remove %s: %s", dirFile, e.msg);
-				}
-			}
-		} catch(Exception e) {
-			this.output("Unable to traverse %s: %s", srcPath, e.msg);
-		}
-	}
-}
+import std.parallelism: parallel;
+import std.string: format;
+import std.stdio: stdout;
+import std.conv: to;
+import std.experimental.logger: FileLogger;
 
 void main(string[] args) {
 	bool verbose;
-	string outFile;
+	string logFile;
+
 	auto cliOptions = getopt(args,
 			std.getopt.config.caseSensitive,
 			std.getopt.config.passThrough,
-			"verbose|v", "If set, each file is listed in the output", &verbose,
-			"output|o", "If set, the results will be output to a given file", &outFile
+			"verbose|v", "If set, each file is listed in the log", &verbose,
+			"log", "If set, the results will be logged to a given file", &logFile
 	);
 	if (cliOptions.helpWanted) {
-		defaultGetoptPrinter(format("Usage: %s [options] [sourcePath]. If sourcePath is not given, the cleaner will assume it's placed in theQube\tools folder.\nAvailable options:", args[0]), cliOptions.options);
+		defaultGetoptPrinter(format("Usage: %s [options].\nAvailable options:", args[0]), cliOptions.options);
 	} else {
-		srcPath = args.length < 2? "../src": args[1];
-		auto cleaner = new SourceCleaner(verbose, outFile);
+		auto logging = (logFile !is null && logFile != "")? new FileLogger(logFile): new FileLogger(stdout);
 		try {
-			cleaner.clean(srcPath);
+			auto dirIter = dirEntries("..", "{*.pyc,*.pyo,*.obj}", SpanMode.depth);
+			foreach(dirFile; parallel(dirIter, 1)) {
+				try {
+					remove(dirFile);
+					if (verbose) {
+						logging.infof("%s: removed", dirFile);
+					}
+				} catch(Exception e) {
+					logging.errorf("Unable to remove %s: %s", dirFile, to!string(e));
+				}
+			}
 		} catch(Exception e) {
-			cleaner.output("Unable to process %s: %s", srcPath, e.msg);
+			logging.errorf("Unable to traverse TheQube source folder: %s", to!string(e));
 		}
 	}
 }
